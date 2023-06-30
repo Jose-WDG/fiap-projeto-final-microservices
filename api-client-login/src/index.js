4f565483764d61b778c225fe606f0f9dc6d38fb1
Jose-WDG <neto.silvacem@gmail.com>
2023-06-29 21:59:34 -0300

    inicializando index

--- api-client-login/src/index.js
+++ api-client-login/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.send('Api para controle dos usuários');
 });
 
 const port = 4000;
